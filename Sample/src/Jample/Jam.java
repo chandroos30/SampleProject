d6f57ca6669cc10ada6e6f4a6d3e95e1637f46f5
chandroos30 <chandroos30@gmail.com>
2023-02-04 23:01:58 +0530

    5th commit

--- Sample/src/Jample/Jam.java
+++ Sample/src/Jample/Jam.java
@@ -2,9 +2,12 @@ package Jample;
 
 public class Jam {
 	
-	jam
+	public void jam() {
+		System.out.println("Hi its jam");
+	}
 
 	public static void main(String[] args) {
 		Jam j = new Jam();
+		j.jam();
 	}
 }
